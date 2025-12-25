5de8505449183ff026cd470f245256c1648a3460
Novikov.MN <Novikov.MN@esphere.ru>
2025-12-25 21:02:33 +0300

    замена на div table

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,8 @@
 'use strict';
 
 $(document).ready(function() {
-    // Для каждой таблицы ищем свою кнопку и строки
     $('.toggle-table').each(function() {
         var $wrapper = $(this);
-        console.log($wrapper);
         var $rows = $wrapper.find('.toggle-table-item.active');
         var $toggleButton = $wrapper.find('.toggle-table-btn');
         var opened = false;
@@ -12,12 +10,10 @@ $(document).ready(function() {
         $toggleButton.on('click', function() {
             $rows.toggleClass('hidden');
             opened = !opened;
-            // Меняем текст кнопки в зависимости от состояния
             var openText = $toggleButton.data('open');
             var closeText = $toggleButton.data('close');
             $toggleButton.text(opened ? closeText : openText);
         });
-        // По умолчанию скрываем строки
         $rows.addClass('hidden');
     });
 });
