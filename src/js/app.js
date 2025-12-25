0256d41f234176fab3da5177b5d79754e94973ae
michail <novikov@perco.ru>
2025-12-25 17:51:15 +0300

    add

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,21 @@
 'use strict';
 
 $(document).ready(function() {
-  
+    var rows = document.querySelectorAll('tr');
+     console.log('rows');
+
+    document.getElementById('skd-web').addEventListener('click', function(event) {
+
+        if( rows.classList === 'active' ) {
+            hiddenRows.forEach(function(row) {
+                row.classList.toggle('hidden');
+            });
+        }
+
+        if (event.target.id === 'toggleButton') {
+            hiddenRows.forEach(function(row) {
+                row.classList.toggle('hidden');
+            });
+        }
+    });
 });
