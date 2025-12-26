fb81b6f1c365f77d51633ab9d96e491beb7f9665
michail <novikov@perco.ru>
2025-12-26 15:20:38 +0300

    add

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,16 +4,20 @@ $(document).ready(function() {
     $('.toggle-section').each(function() {
         var $wrapper = $(this);
         var $rows = $wrapper.find('.toggle-section-item');
-        var $toggleButton = $wrapper.find('.toggle-section-btn');
+        var $toggleButton = $wrapper.find('.js-toggle-btn');
         var opened = false;
 
         $toggleButton.on('click', function() {
-            $rows.toggleClass('active');
-            opened = !opened;
             var openText = $toggleButton.data('open');
             var closeText = $toggleButton.data('close');
-            $toggleButton.text(opened ? closeText : openText);
+
+            $rows.toggleClass('hidden');
+            opened = !opened;
+            $toggleButton
+                .text(opened ? closeText : openText)
+                .toggleClass('opened');
         });
-        $rows.addClass('active');
+
+        $rows.addClass('hidden')
     });
 });
